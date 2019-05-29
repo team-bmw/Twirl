3c7a49df05d04c89da8486c2b65a3154102335ff
Jacky Wong <jackyw2017@gmail.com>
2019-05-28 21:20:18 -0400

    front-end first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Landing from './Landing';
+
+import { HashRouter as Router, Route } from 'react-router-dom';
+import WordCloud from './WordCloud';
 
 const App = () => {
   return (
-    <div>
-      Hello world
-    </div>
+    <Router>
+      <Route exact path="/" component={Landing} />
+      <Route path="/search" component={WordCloud} />
+    </Router>
   );
 };
 export default App;
---
+++ src/components/WordCloud/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WordCloud = () => {
+  return <div>WordCloud</div>;
+};
+
+export default WordCloud;
