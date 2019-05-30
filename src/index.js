db5ef1a8740e33cf73cbcfe90d969fd6e2ce9197
Jacky Wong <jackyw2017@gmail.com>
2019-05-30 09:12:56 -0400

    wordcloud first commit; wordpos installed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store from './store';
 import App from './components/App';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.querySelector('#app')
-);
+ReactDOM.render(<App />, document.querySelector('#app'));
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { wordcloudData } from './wordcloudReducer';
-
-const rootReducer = combineReducers({
-  wordcloudData,
-});
-
-export default rootReducer;
