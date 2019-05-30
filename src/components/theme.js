415ab097e27db3395e7c2d80e239f21a78626149
Jacky Wong <jackyw2017@gmail.com>
2019-05-30 19:59:11 -0400

    materialUI_wordpos

---
+++ src/components/theme.js
@@ -0,0 +1,22 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+import blue from '@material-ui/core/colors/blue';
+import blueGrey from '@material-ui/core/colors/blueGrey';
+
+// setting up the theme for Material-UI (will be updated later)
+export default createMuiTheme({
+  palette: {
+    primary: {
+      light: blue[100],
+      main: blue[500],
+      dark: blue[900],
+      contrastText: blue[50],
+    },
+    secondary: {
+      light: blueGrey[100],
+      main: blueGrey[500],
+      dark: blueGrey[900],
+      contrastText: blueGrey[50],
+    },
+  },
+});
