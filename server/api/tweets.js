77c124e41f1a2eb93b6f538af6dd3bb80a545c3e
Jacky Wong <jackyw2017@gmail.com>
2019-06-04 21:25:21 -0400

    wordcloud refactoring

--- server/api/tweets.js
+++ server/api/tweets.js
@@ -1,27 +1,30 @@
-
 const router = require('express').Router();
 const { fetchTweets } = require('../twitter/fetchTweets');
-const { tweetsToWordFrequencies, adjectivesToWordFrequencies, nounsToWordFrequencies } = require('../twitter/parseTweets');
+const {
+  tweetsToWordFrequencies,
+  adjectivesToWordFrequencies,
+  nounsToWordFrequencies,
+} = require('../twitter/parseTweets');
 
 // fetch and send 500 tweets, using query string
 router.get('/:query', (req, res, next) => {
-    fetchTweets(req.params.query, 500)
-        .then(tweets => res.send(tweetsToWordFrequencies(tweets)))
-        .catch(err => console.log(err));
+  fetchTweets(req.params.query, 100)
+    .then(tweets => res.send(tweetsToWordFrequencies(tweets)))
+    .catch(err => console.log(err));
 });
 
 router.get('/adjectives/:query', (req, res, next) => {
-    fetchTweets(req.params.query, 500)
-        .then(tweets => adjectivesToWordFrequencies(tweets))
-        .then(adj => res.send(adj))
-        .catch(err => console.log(err));
+  fetchTweets(req.params.query, 500)
+    .then(tweets => adjectivesToWordFrequencies(tweets))
+    .then(adj => res.send(adj))
+    .catch(err => console.log(err));
 });
 
 router.get('/nouns/:query', (req, res, next) => {
-    fetchTweets(req.params.query, 500)
-        .then(tweets => nounsToWordFrequencies(tweets))
-        .then(adj => res.send(adj))
-        .catch(err => console.log(err));
+  fetchTweets(req.params.query, 500)
+    .then(tweets => nounsToWordFrequencies(tweets))
+    .then(adj => res.send(adj))
+    .catch(err => console.log(err));
 });
 
 module.exports = router;
