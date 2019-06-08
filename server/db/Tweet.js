b510038d7356574c70636c0301395f7b4d7a28b5
Justine Morgan <justinemorgan11@gmail.com>
2019-06-07 21:02:19 -0400

    updated tweet model and fetch functions to get more fields from tweets

--- server/db/Tweet.js
+++ server/db/Tweet.js
@@ -4,12 +4,33 @@ const Tweet = db.define('tweets', {
     text: {
         type: db.Sequelize.TEXT,
     },
+    location: {
+        type: db.Sequelize.TEXT,
+    },
+    numFollowers: {
+        type: db.Sequelize.INTEGER,
+    },
+    numFriends: {
+        type: db.Sequelize.INTEGER,
+    },
+    numFavorites: {
+        type: db.Sequelize.INTEGER,
+    },
+    numRetweets: {
+        type: db.Sequelize.INTEGER,
+    },
+    userVerified: {
+        type: db.Sequelize.BOOLEAN,
+    },
     query: {
         type: db.Sequelize.STRING,
     },
     twitterId: {
         type: db.Sequelize.STRING,
-    }
+    },
+    twitterUserId: {
+        type: db.Sequelize.STRING,
+    },
 });
 
 module.exports = Tweet;
--- server/twitter/fetchTweets.js
+++ server/twitter/fetchTweets.js
@@ -6,7 +6,7 @@
 const Twitter = require('twitter');
 require('dotenv').config();
 
-const { db, Tweet, Metadata } = require('../db/index');
+const { Tweet, Metadata } = require('../db/index');
 
 // Change out keys here:
 const client = new Twitter({
@@ -41,7 +41,14 @@ const getTweets = async (q, count, max_id = null) => {
     Tweet.create({
       query: q,
       text: element.full_text,
+      location: element.user.location,
+      numFollowers: element.user.followers_count,
+      numFriends: element.user.friends_count,
+      numFavorites: element.favorite_count,
+      numRetweets: element.retweet_count,
+      userVerified: element.user.verified,
       twitterId: `${element.id_str}`,
+      twitterUserId: element.user.id_str,
     });
   });
 
