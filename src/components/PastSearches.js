af24a9dbc4eb94fd281b2e32dbf5270c61a7e45b
Jacky Wong <jackyw2017@gmail.com>
2019-06-17 09:17:07 -0400

    toggle-chart buttons now work

--- src/components/PastSearches.js
+++ src/components/PastSearches.js
@@ -14,91 +14,97 @@ import InputLabel from '@material-ui/core/InputLabel';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        color: 'white',
-    },
-    formControl: {
-        margin: theme.spacing(1),
-        minWidth: 120,
-    },
-    selectEmpty: {
-        marginTop: theme.spacing(2),
-    },
-    labelFormatting: {
-        color: 'white',
-    },
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    color: 'white',
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+  labelFormatting: {
+    color: 'white',
+  },
 }));
 
 const PastSearches = ({
-    selectSearchId,
-    fetchAdjectiveWordcloudData,
-    searches,
-    emptySelectedTweets,
-    emptyRemovedWords,
+  selectSearchId,
+  fetchAdjectiveWordcloudData,
+  searches,
+  emptySelectedTweets,
+  emptyRemovedWords,
 }) => {
+  const classes = useStyles();
+  const [values, setValues] = React.useState({
+    search_id: '',
+  });
 
-    const classes = useStyles();
-    const [values, setValues] = React.useState({
-        search_id: '',
-    });
+  const handleChange = ({ target }) => {
+    setValues(oldValues => ({
+      ...oldValues,
+      [target.name]: target.value,
+    }));
+    selectSearchId(Number(target.value));
+    fetchAdjectiveWordcloudData(Number(target.value));
+    selectSearchId(target.value);
+    emptyRemovedWords();
+    emptySelectedTweets();
+  };
 
-    const handleChange = ({ target }) => {
-        setValues(oldValues => ({
-            ...oldValues,
-            [target.name]: target.value,
-        }));
-        selectSearchId(Number(target.value));
-        fetchAdjectiveWordcloudData(Number(target.value));
-        selectSearchId(target.value);
-        emptyRemovedWords();
-        emptySelectedTweets();
-    }
-
-    return (
-        <div>
-            <form className={classes.root} autoComplete="off">
-                <FormControl className={classes.formControl}>
-                    <InputLabel className={classes.labelFormatting} htmlFor="id-simple">Past Searches</InputLabel>
-                    <Select
-                        className={classes.labelFormatting}
-                        value={values.search_id}
-                        onChange={handleChange}
-                        inputProps={{
-                            name: 'search_id',
-                            id: 'id-simple',
-                        }}
-                    >
-                        <MenuItem value="">
-                            <em>None</em>
-                        </MenuItem>
-                        {searches.searches.map(search => {
-                            return (
-                                <MenuItem key={search.search_id} value={search.search_id}>{search.query}</MenuItem>
-                            )
-                        })}
-                    </Select>
-                </FormControl>
-            </form>
-        </div>
-    )
-}
+  return (
+    <div>
+      <form className={classes.root} autoComplete="off">
+        <FormControl className={classes.formControl}>
+          <InputLabel className={classes.labelFormatting} htmlFor="id-simple">
+            Past Searches
+          </InputLabel>
+          <Select
+            className={classes.labelFormatting}
+            value={values.search_id}
+            onChange={handleChange}
+            inputProps={{
+              name: 'search_id',
+              id: 'id-simple',
+            }}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            {searches.searches.map(search => {
+              return (
+                <MenuItem key={search.search_id} value={search.search_id}>
+                  {search.query}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+      </form>
+    </div>
+  );
+};
 
 const mapStateToProps = ({ searches }) => {
-    return {
-        searches,
-    }
-}
+  return {
+    searches,
+  };
+};
 
 const mapDispatchToProps = dispatch => {
-    return {
-        selectSearchId: search_id => dispatch(selectSearchId(search_id)),
-        fetchAdjectiveWordcloudData: search_id => dispatch(fetchAdjectiveWordcloudData(search_id)),
-        emptySelectedTweets: () => dispatch(emptySelectedTweets()),
-        emptyRemovedWords: () => dispatch(emptyRemovedWords()),
-    }
-}
+  return {
+    selectSearchId: search_id => dispatch(selectSearchId(search_id)),
+    fetchAdjectiveWordcloudData: search_id =>
+      dispatch(fetchAdjectiveWordcloudData(search_id)),
+    emptySelectedTweets: () => dispatch(emptySelectedTweets()),
+    emptyRemovedWords: () => dispatch(emptyRemovedWords()),
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(PastSearches);
-
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PastSearches);
