c267305e36e478317f25ee8315ec49cd15ad29bb
Alex Bukhman <alexb823@gmail.com>
2019-06-19 21:54:34 -0400

    top 10 word showing on the line chart.  refactored BarChart

--- src/components/BarChart/index.js
+++ src/components/BarChart/index.js
@@ -1,8 +1,7 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import { ResponsiveBar } from '@nivo/bar';
 import { makeStyles } from '@material-ui/core/styles';
-import { useTheme } from '@material-ui/styles';
 import {
   selectWordElement,
   selectCloudWord,
@@ -10,6 +9,7 @@ import {
 
 import WordPopover from '../Search/WordPopover';
 import ColorSpectrum from '../Search/ColorSpectrum';
+import { getBarChartData } from './utils';
 
 // make sure parent container have a defined height when using
 // responsive component, otherwise height will be 0 and
@@ -24,39 +24,6 @@ const useStyles = makeStyles(theme => ({
 
 const BarChart = ({ wordData, selectWordElement, selectCloudWord }) => {
   const classes = useStyles();
-  const theme = useTheme();
-  const {
-    palette: { grey, blue },
-  } = theme;
-
-  //Helper func to get the word sentiment color
-  const getWordColor = tweetData => {
-    const sentimentSum = tweetData.reduce((sum, { sentiment }) => {
-      sum += sentiment;
-      return sum;
-    }, 0);
-    const sentimentAverage = sentimentSum / tweetData.length;
-    if (sentimentAverage > 0.8) return grey['50'];
-    else if (sentimentAverage > 0.6) return blue['100'];
-    else if (sentimentAverage > 0.4) return blue['300'];
-    else if (sentimentAverage > 0.2) return blue['700'];
-    else return blue['900'];
-  };
-
-  //Helper funcs to format the data for ResponsiveBar
-  let barChartDate = wordData
-    .map(wordObj => {
-      return {
-        text: wordObj.text,
-        tweets: wordObj.value,
-        [wordObj.text + 'Color']: getWordColor(wordObj.tweetData),
-        cloudWord: wordObj,
-      };
-    })
-    .sort((a, b) => a.tweets - b.tweets);
-
-  // Max of top 40 word will be shown in the bar chart
-  if (barChartDate.length > 39) barChartDate = barChartDate.slice(-40);
 
   return (
     <Fragment>
@@ -64,7 +31,7 @@ const BarChart = ({ wordData, selectWordElement, selectCloudWord }) => {
         <ResponsiveBar
           enableGridY={false}
           enableGridX={true}
-          data={barChartDate}
+          data={getBarChartData(wordData)}
           keys={['tweets']}
           indexBy="text"
           margin={{ top: 50, right: 50, bottom: 50, left: 100 }}
--- src/components/LineChartTime/index.js
+++ src/components/LineChartTime/index.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import { ResponsiveLine } from '@nivo/line';
 import Loading from '../Common/Loading';
+import {getTopWords} from './utils';
 
 // make sure parent container have a defined height when using
 // responsive component, otherwise height will be 0 and
@@ -15,7 +16,6 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-
 const LineChartTime = props => {
   const classes = useStyles();
   // console.log(props);
@@ -26,83 +26,80 @@ const LineChartTime = props => {
   if (status === 'fetched' && wordData.length) {
     return (
       <div className={classes.lineChart}>
-          <ResponsiveLine
-              data={wordData}
-              margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-              xScale={{
-              type: 'time',
-              format: '%Y-%m-%d',
-              precision: 'day',
-              useUTC: false,
-              }}
-              xFormat="time:%Y-%m-%d"
-              
-              yScale={{type: 'linear', stacked: false}}
-              axisLeft={{
-                orient: 'left',
-                tickSize: 5,
-                tickPadding: 10,
-                tickRotation: 0,
-                legendOffset: -40,
-                legend: 'Number of Tweets',
-                legendPosition: 'middle',
-              }}
-              
-              axisBottom={{
-                  format: '%b %d',
-                  orient: 'bottom',
-                  tickSize: 5,
-                  tickPadding: 5,
-                  tickRotation: 0,
-                  legend: 'Date',
-                  legendOffset: 36,
-                  legendPosition: 'middle',
-                  tickValues: 7,
-              }}
-              curve="cardinal"
-              axisTop={null}
-              axisRight={null}
-              colors={{ scheme: 'category10' }}
-              pointSize={12}
-              pointColor={{ from: 'color' }}
-              pointBorderWidth={2}
-              pointBorderColor={{ from: 'color', modifiers: [['darker', 0.5]] }}
-              pointLabel="y"
-              pointLabelYOffset={-12}
-              enableSlices={'x'}
-              legends={[
-                  {
-                      anchor: 'bottom-right',
-                      direction: 'column',
-                      justify: false,
-                      translateX: 100,
-                      translateY: 0,
-                      itemsSpacing: 0,
-                      itemDirection: 'left-to-right',
-                      itemWidth: 80,
-                      itemHeight: 20,
-                      itemOpacity: 0.75,
-                      symbolSize: 12,
-                      symbolShape: 'circle',
-                      symbolBorderColor: 'rgba(0, 0, 0, .5)',
-                      effects: [
-                          {
-                              on: 'hover',
-                              style: {
-                                  itemBackground: 'rgba(0, 0, 0, .03)',
-                                  itemOpacity: 1
-                              }
-                          }
-                      ]
-                  }
-              ]}
-          />
-    </div>
-    )
-  }
-  else if (status === 'fetched' && !wordData.length) return null;
+        <ResponsiveLine
+          data={getTopWords(wordData)}
+          margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+          xScale={{
+            type: 'time',
+            format: '%Y-%m-%d',
+            precision: 'day',
+            useUTC: false,
+          }}
+          xFormat="time:%Y-%m-%d"
+          yScale={{ type: 'linear', stacked: false }}
+          axisLeft={{
+            orient: 'left',
+            tickSize: 5,
+            tickPadding: 10,
+            tickRotation: 0,
+            legendOffset: -40,
+            legend: 'Number of Tweets',
+            legendPosition: 'middle',
+          }}
+          axisBottom={{
+            format: '%b %d',
+            orient: 'bottom',
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: 'Date',
+            legendOffset: 36,
+            legendPosition: 'middle',
+            tickValues: 7,
+          }}
+          curve="cardinal"
+          axisTop={null}
+          axisRight={null}
+          colors={{ scheme: 'category10' }}
+          pointSize={12}
+          pointColor={{ from: 'color' }}
+          pointBorderWidth={2}
+          pointBorderColor={{ from: 'color', modifiers: [['darker', 0.5]] }}
+          pointLabel="y"
+          pointLabelYOffset={-12}
+          enableSlices={'x'}
+          legends={[
+            {
+              anchor: 'bottom-right',
+              direction: 'column',
+              justify: false,
+              translateX: 100,
+              translateY: 0,
+              itemsSpacing: 0,
+              itemDirection: 'left-to-right',
+              itemWidth: 80,
+              itemHeight: 20,
+              itemOpacity: 0.75,
+              symbolSize: 12,
+              symbolShape: 'circle',
+              symbolBorderColor: 'rgba(0, 0, 0, .5)',
+              effects: [
+                {
+                  on: 'hover',
+                  style: {
+                    itemBackground: 'rgba(0, 0, 0, .03)',
+                    itemOpacity: 1,
+                  },
+                },
+              ],
+            },
+          ]}
+        />
+      </div>
+    );
+  } else if (status === 'fetched' && !wordData.length) return null;
   else return <Loading />;
-}
+};
 
 const mapStateToProps = state => state;
 
